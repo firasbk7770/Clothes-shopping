2ddefc74ffb05080a96894277d8c6b87017dc285
Andrii Mandziuk <160692511+AndriiMandziuk@users.noreply.github.com>
2024-05-31 11:38:20 +0300

    fix private route

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,6 +1,5 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
 const PrivateRoute = () => {
   const { userInfo } = useSelector((state) => state.auth);
   return userInfo ? <Outlet /> : <Navigate to='/login' replace />;
